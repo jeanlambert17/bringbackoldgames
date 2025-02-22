678a776d59bc5ca22792552205d001b0e9af1234
Jean Lambert <jclrdev@gmail.com>
2025-02-21 19:06:56 -0500

    Hide router dev tools

--- src/components/layout/RootLayout.tsx
+++ src/components/layout/RootLayout.tsx
@@ -1,6 +1,6 @@
 import { AuthProvider } from '@/contexts/auth.context'
 import { Outlet } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+// import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 import { Toaster } from '@/components/ui/toaster'
 import { useDarkMode } from '@/hooks/use-dark-mode'
 
@@ -11,7 +11,7 @@ export function RootLayout() {
     <AuthProvider>
       <div className="min-h-screen bg-background">
         <Outlet />
-        <TanStackRouterDevtools />
+        {/* <TanStackRouterDevtools /> */}
       </div>
       <Toaster />
     </AuthProvider>
